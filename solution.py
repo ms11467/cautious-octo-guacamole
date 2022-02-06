fe155b1c3c766cd8d0d91ee836fd992914a41e13
Madhu Sanapala <ms11467@nyu.edu>
2022-02-06 14:41:50 -0500

    third commit

--- solution.py
+++ solution.py
@@ -1,41 +1,26 @@
-import hashlib
+### welcome_assignment_answers
+### Input - All eight questions given in the assignment.
+### Output - The right answer for the specific question.
 
-print(hashlib.algorithms_available)
-print(hashlib.algorithms_guaranteed)
-print(" ")
-
-t = "NYU Computer Networking"
-m = hashlib.md5(t.encode()).hexdigest()
-s = hashlib.sha256(t.encode()).hexdigest()
-# print('MD5 hash generator: ', m)
 
 def welcome_assignment_answers(question):
   if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?:":
-   print(question)
    answer = "mtls"
   elif question == "Are encoding and encryption the same? - Yes/No:":
-   print(question)
    answer = "No"
   elif question == "Is it possible to decrypt a message without a key? - Yes/No:":
-   print(question)
    answer = "No"
   elif question == "Is it possible to decode a message without a key? - Yes/No:":
-   print(question)
-   answer = "Yes"
+   answer = "No"
   elif question == "Is a hashed message supposed to be un-hashed? - Yes/No:":
-   print(question)
    answer = "No"
   elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking':":
-   print(question)
-   answer = (m)
+   answer = "42b76fe51778764973077a5a94056724"
   elif question == "Is MD5 a secured hashing algorithm? - Yes/No:":
-   print(question)
    answer = "Yes"
   elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? (Numeric):":
-   print(question)
    answer = "3"
   elif question == "What layer of the TCP/IP model the protocol TCP belongs to? (Numeric):":
-   print(question)
    answer = "4"
   return answer
   
@@ -51,7 +36,7 @@ if __name__ == "__main__":
    debug_question = "Is a hashed message supposed to be un-hashed? - Yes/No:"
    print(welcome_assignment_answers(debug_question))
    debug_question = "What is the MD5 hashing value to the following message: 'NYU Computer Networking':"
-   print("MD Hashing =", welcome_assignment_answers(debug_question))
+   print(welcome_assignment_answers(debug_question))
    debug_question = "Is MD5 a secured hashing algorithm? - Yes/No:"
    print(welcome_assignment_answers(debug_question))
    debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? (Numeric):"
