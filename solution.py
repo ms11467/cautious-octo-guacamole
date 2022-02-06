50133e44c00f50c2019268a85c7731077d29dce1
Madhu Sanapala <ms11467@nyu.edu>
2022-02-06 14:02:49 -0500

    second commit

--- solution.py
+++ solution.py
@@ -1,6 +1,5 @@
 import hashlib
 
-
 print(hashlib.algorithms_available)
 print(hashlib.algorithms_guaranteed)
 print(" ")
@@ -10,64 +9,52 @@ m = hashlib.md5(t.encode()).hexdigest()
 s = hashlib.sha256(t.encode()).hexdigest()
 # print('MD5 hash generator: ', m)
 
-'''
-### welcome_assignment_answers
-### Input - All eight questions given in the assignment.
-### Output - The right answer for the specific question.
-
-#def welcome_assignment_answers(question):
-    #Students do not have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-'''
-question = input("In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?:")
-if question == "mtls":
-   print("Answer = mtls")
-else:
-   print("Try Again")
-question = input("Are encoding and encryption the same? - Yes/No:")
-if question == "No":
-   print("Answer = No")
-else:
-   print("Try Again")
-question = input("Is it possible to decrypt a message without a key? - Yes/No:")   
-if question == "No":
-   print("Answer = No")
-else:
-   print("Try Again")  
-question = input("Is it possible to decode a message without a key? - Yes/No:")
-if question == "Yes":
-   print("Answer = Yes")
-else:
-   print("Try Again")   
-question = input("Is a hashed message supposed to be un-hashed? - Yes/No:") 
-if question == "No":
-   print("Answer = No")
-else:
-   print("Try Again")   
-question = input("What is the MD5 hashing value to the following message: 'NYU Computer Networking':")
-if question == "md5":
-   print("MD5 hasing value =", m)
-else:
-   print("Try Again") 
-question = input("Is MD5 a secured hashing algorithm? - Yes/No:")
-if question == "Yes":
-   print("Answer = Yes")
-else:
-   print("Try Again")   
-question = input("What layer from the TCP/IP model the protocol DHCP belongs to? (Numeric): ")
-if question == "3":
-   print("Answer = 3")
-else:
-   print("Try Again, Require a Numeric value")   
-question = input("What layer of the TCP/IP model the protocol TCP belongs to? (Numeric): ")
-if question == "4":
-   print("Answer = 4")
-else:
-   print("Try Again, Require a Numeric value") 
-
-'''
+def welcome_assignment_answers(question):
+  if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?:":
+   print(question)
+   answer = "mtls"
+  elif question == "Are encoding and encryption the same? - Yes/No:":
+   print(question)
+   answer = "No"
+  elif question == "Is it possible to decrypt a message without a key? - Yes/No:":
+   print(question)
+   answer = "No"
+  elif question == "Is it possible to decode a message without a key? - Yes/No:":
+   print(question)
+   answer = "Yes"
+  elif question == "Is a hashed message supposed to be un-hashed? - Yes/No:":
+   print(question)
+   answer = "No"
+  elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking':":
+   print(question)
+   answer = (m)
+  elif question == "Is MD5 a secured hashing algorithm? - Yes/No:":
+   print(question)
+   answer = "Yes"
+  elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? (Numeric):":
+   print(question)
+   answer = "3"
+  elif question == "What layer of the TCP/IP model the protocol TCP belongs to? (Numeric):":
+   print(question)
+   answer = "4"
+  return answer
+  
 if __name__ == "__main__":
-    #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No: "
-    print(welcome_assignment_answers(debug_question))
-'''  
+   debug_question = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?:"
+   print(welcome_assignment_answers(debug_question))
+   debug_question = "Are encoding and encryption the same? - Yes/No:"
+   print(welcome_assignment_answers(debug_question))
+   debug_question = "Is it possible to decrypt a message without a key? - Yes/No:"
+   print(welcome_assignment_answers(debug_question))
+   debug_question = "Is it possible to decode a message without a key? - Yes/No:"
+   print(welcome_assignment_answers(debug_question))
+   debug_question = "Is a hashed message supposed to be un-hashed? - Yes/No:"
+   print(welcome_assignment_answers(debug_question))
+   debug_question = "What is the MD5 hashing value to the following message: 'NYU Computer Networking':"
+   print("MD Hashing =", welcome_assignment_answers(debug_question))
+   debug_question = "Is MD5 a secured hashing algorithm? - Yes/No:"
+   print(welcome_assignment_answers(debug_question))
+   debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? (Numeric):"
+   print(int(welcome_assignment_answers(debug_question)))
+   debug_question = "What layer of the TCP/IP model the protocol TCP belongs to? (Numeric):"
+   print(int(welcome_assignment_answers(debug_question)))
