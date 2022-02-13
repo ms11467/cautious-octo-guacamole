db153ff5afa2964fe4c3ac436abf2fff84451d8d
ms11467 <99107740+ms11467@users.noreply.github.com>
2022-02-13 12:51:20 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -22,10 +22,11 @@ def webServer(port=13331):
           filename = message.split()[1]
           f = open(filename[1:])
           outputdata = f.read                         #Fill in start     #Fill in end
-          f.close()
+          #f.close()
           #Send one HTTP header line into socket.
           #Fill in start
-          connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n')
+          okMessage = 'HTTP/1/1 200 OK \r\n'
+          connectionSocket.send(okMessage.encode())   #OK message ready to send
           #Fill in end
           #Send the content of the requested file to the client
           for i in range(0, len(outputdata)):
