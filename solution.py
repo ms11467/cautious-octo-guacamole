6e860b2d1d5195d72dbadfd213061956ab5153f8
ms11467 <99107740+ms11467@users.noreply.github.com>
2022-02-19 11:59:56 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ from socket import *
 import sys
 
 
-def webServer(port=56226):
+def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
   serverSocket.bind(("", port)) 
@@ -54,4 +54,4 @@ def webServer(port=56226):
   sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
-  webServer(56226)
+  webServer(13331)
