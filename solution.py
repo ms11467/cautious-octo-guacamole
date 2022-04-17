9986feedc7bf5a26b28cdff29288537a654f1024
ms11467 <99107740+ms11467@users.noreply.github.com>
2022-04-16 22:36:18 -0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -121,9 +121,10 @@ def ping(host, timeout=1):
         time.sleep(1)  # one second
         
     #You should have the values of delay for each ping here; fill in calculation for packet_min, packet_avg, packet_max, and stdev
-    #vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
+    vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
 
     return vars
+#    return delay
 
 
 if __name__ == '__main__':
